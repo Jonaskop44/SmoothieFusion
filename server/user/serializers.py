d26774e69713777b8f9134e8ad1ce1f91082a70f
jonaskop44 <jonas@codeflexx.com>
2025-04-18 18:08:48 +0200

    changes

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -6,7 +6,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'created_at', 'updated_at']
+        fields = ['id', 'username', 'email', 'password', 'created_at', 'updated_at']
         read_only_fields = ['id', 'created_at', 'updated_at']
 
     def validate(self, data):
