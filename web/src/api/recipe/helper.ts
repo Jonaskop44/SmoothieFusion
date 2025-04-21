fdce421ce6f6d300f1afb037f02e603ceef36d17
jonaskop44 <jonas@codeflexx.com>
2025-04-21 21:49:05 +0200

    changes

--- web/src/api/recipe/helper.ts
+++ web/src/api/recipe/helper.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from "@/types/recipe";
 import axios from "axios";
 
 export class Helper {
@@ -22,6 +23,102 @@ export class Helper {
       .get(`/recipes/${id}/`)
       .then((response) => {
         if (response.status !== 200) return { data: null, status: false };
+
+        const data = response.data;
+        return { data: data, status: true };
+      })
+      .catch(() => {
+        return { data: null, status: false };
+      });
+  }
+
+  async createRecipe(
+    image: File,
+    name: string,
+    instructions: string,
+    ingredients: Ingredient[]
+  ) {
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("name", name);
+    formData.append("instructions", instructions);
+    formData.append("ingredients", JSON.stringify(ingredients));
+
+    return axios
+      .post("/recipes/create/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        if (response.status !== 201) return { data: null, status: false };
+
+        const data = response.data;
+        return { data: data, status: true };
+      })
+      .catch(() => {
+        return { data: null, status: false };
+      });
+  }
+
+  async updateRecipe(
+    id: number,
+    image?: File,
+    name?: string,
+    instructions?: string,
+    ingredients?: Ingredient[]
+  ) {
+    const formData = new FormData();
+    if (image) formData.append("image", image);
+    if (name) formData.append("name", name);
+    if (instructions) formData.append("instructions", instructions);
+    if (ingredients)
+      formData.append("ingredients", JSON.stringify(ingredients));
+
+    return axios
+      .patch(`/recipes/update/${id}/`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        if (response.status !== 200) return { data: null, status: false };
+
+        const data = response.data;
+        return { data: data, status: true };
+      })
+      .catch(() => {
+        return { data: null, status: false };
+      });
+  }
+
+  async deleteRecipe(id: number) {
+    return axios
+      .delete(`/recipes/delete/${id}/`)
+      .then((response) => {
+        if (response.status !== 204) return { data: null, status: false };
+
+        return { data: null, status: true };
+      })
+      .catch(() => {
+        return { data: null, status: false };
+      });
+  }
+
+  async createReview(
+    id: number,
+    name: string,
+    description: string,
+    rating: number
+  ) {
+    return axios
+      .post(`/recipes/${id}/reviews/`, {
+        name: name,
+        description: description,
+        rating: rating,
+      })
+      .then((response) => {
+        if (response.status !== 201) return { data: null, status: false };
 
         const data = response.data;
         return { data: data, status: true };
---
+++ web/src/data/recipeStore.ts
@@ -0,0 +1,99 @@
+import { create } from "zustand";
+import ApiClient from "@/api";
+import { Ingredient, Recipe } from "@/types/recipe";
+
+interface RecipeState {
+  recipes: Recipe[];
+  fetchRecipes: () => void;
+  createRecipe: (
+    image: File,
+    name: string,
+    instructions: string,
+    ingredients: Ingredient[]
+  ) => void;
+  updateRecipe: (
+    id: number,
+    image?: File,
+    name?: string,
+    instructions?: string,
+    ingredients?: Ingredient[]
+  ) => void;
+  deleteRecipe: (id: number) => void;
+}
+
+const apiClient = new ApiClient();
+
+export const recipeStore = create<RecipeState>((set) => ({
+  recipes: [] as Recipe[],
+  fetchRecipes: () => {
+    return apiClient.recipe.helper
+      .getAllRecipes()
+      .then((response) => {
+        if (response.status) {
+          set({ recipes: response.data });
+        } else {
+          set({ recipes: [] as Recipe[] });
+        }
+      })
+      .catch(() => {
+        set({ recipes: [] as Recipe[] });
+      });
+  },
+  createRecipe: (
+    image: File,
+    name: string,
+    instructions: string,
+    ingredients: Ingredient[]
+  ) => {
+    return apiClient.recipe.helper
+      .createRecipe(image, name, instructions, ingredients)
+      .then((response) => {
+        if (response.status) {
+          set((state) => ({ recipes: [...state.recipes, response.data] }));
+        }
+      })
+      .catch(() => {
+        set((state) => ({ recipes: [...state.recipes] }));
+      });
+  },
+  updateRecipe: (
+    id: number,
+    image?: File,
+    name?: string,
+    instructions?: string,
+    ingredients?: Ingredient[]
+  ) => {
+    return apiClient.recipe.helper
+      .updateRecipe(id, image, name, instructions, ingredients)
+      .then((response) => {
+        if (response.status) {
+          set((state) => ({
+            recipes: state.recipes.map((r) =>
+              r.id === id ? response.data : r
+            ),
+          }));
+        }
+      })
+      .catch(() => {
+        set((state) => ({
+          recipes: state.recipes.map((r) => (r.id === id ? r : r)),
+        }));
+      });
+  },
+  deleteRecipe: (id: number) => {
+    return apiClient.recipe.helper
+      .deleteRecipe(id)
+      .then((response) => {
+        if (response.status) {
+          set((state) => ({
+            recipes: state.recipes.filter((recipe) => recipe.id !== id),
+          }));
+        }
+      })
+      .catch(() => {
+        set((state) => ({
+          recipes: state.recipes.filter((recipe) => recipe.id !== id),
+        }));
+      });
+  },
+}));
